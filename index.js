d2d1636878f70db0f5cec9a6f668aeadd8aec41d
Adrian <kahuraadrian@gmail.com>
2024-03-21 11:33:35 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+  saturdayFun();
+  
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  
+  function wrapAdjective(adjWrapper = "*") {
+    let part1 = `You are`
+    return function(emphatic) {
+      return `${part1} ${adjWrapper}${emphatic}${adjWrapper}!`;
+    }
+  }
+  console.log(wrapAdjective("*")('strong')); 
+  
